c92c51016702cf68f34b1d9ef59057638654b2cc
Hesam Rad <hesamzakerirad@gmail.com>
2022-08-11 11:15:07 +0430

    feat: added a few macro methods.

--- src/DebuggerServiceProvider.php
+++ src/DebuggerServiceProvider.php
@@ -14,17 +14,12 @@ class DebuggerServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //publish config file
-        $this->publishes([
-            __DIR__ . '/../config/debugger.php' => config_path('debugger.php')
-        ], 'debugger-config');
-
-        //register middleware
-        app('router')->aliasMiddleware('debugger', \HesamRad\Debugger\Middleware\DebuggerMiddleware::class);
-
-        //binding Debugger as a singleton
+        /**
+         * Binding Debugger as a singleton.
+         *
+         */
         $this->app->singleton(Debugger::class, function () {
-            return new Debugger(config('debugger'));
+            return new Debugger();
         });
     }
 
@@ -38,6 +33,16 @@ class DebuggerServiceProvider extends ServiceProvider
         //registering a custom macro
         Request::macro('debug', function () {
             return app(Debugger::class)->debug($this);
+        });
+
+        //registering a custom macro
+        Request::macro('listen', function () {
+            return app(Debugger::class)->listen();
+        });
+
+        //registering a custom macro
+        Request::macro('isBeingDebugged', function () {
+            return app(Debugger::class)->isBeingDebugged();
         });
     }
 }
